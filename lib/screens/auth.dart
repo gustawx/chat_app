762685bf08439720cb96c389099d4ff59959cc7b
sebastian.kustosz.ext@siemens.com <sebastian.kustosz.ext@siemens.com>
2023-12-21 13:58:36 +0100

    feat(build.gradle): enable multiDex in android app to support more than 64K methods feat(auth.dart): add username field to auth form for better user identification feat(auth.dart): store user data in Firestore after successful authentication for future reference

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +24,7 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredPassword = '';
   File? _selectedImage;
   var _isAuthenticating = false;
+  var _enteredUsername;
 
   void _submit() async {
     final isValid = _form.currentState!.validate();
@@ -49,7 +51,15 @@ class _AuthScreenState extends State<AuthScreen> {
 
         await storageRef.putFile(_selectedImage!);
         final imageUrl = await storageRef.getDownloadURL();
-        print(imageUrl);
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userCredentials.user!.uid)
+            .set({
+          'username': _enteredUsername,
+          'email': _enteredEmail,
+          'image_url': imageUrl
+        });
       }
     } on FirebaseAuthException catch (error) {
       // if(error.code == EmailAlrea)
@@ -119,6 +129,23 @@ class _AuthScreenState extends State<AuthScreen> {
                                 _enteredEmail = value!;
                               },
                             ),
+                            if (!_isLogin)
+                              TextFormField(
+                                validator: (value) {
+                                  if (value == null ||
+                                      value.isEmpty ||
+                                      value.trim().length < 4) {
+                                    return 'Username must have at least 4 chars';
+                                  }
+                                  return null;
+                                },
+                                onSaved: (newValue) {
+                                  _enteredUsername = newValue!;
+                                },
+                                decoration: const InputDecoration(
+                                    labelText: 'Username'),
+                                enableSuggestions: false,
+                              ),
                             TextFormField(
                               decoration: const InputDecoration(
                                 label: Text('Password'),
